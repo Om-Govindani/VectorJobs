b5eb346722be24f3fd62cf288bc16120ab3a92d3
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2024-07-04 01:12:48 +0530

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      borderColor:{
+        'custom-color':'rgb(195,199,205)'
+      },
+      backgroundColor: {
+        'back-color': 'rgb(170,210,184)', 
+        'button-color':'rgb(148,195,156)',
+      },
+      colors:{
+        'firstcolor':"rgb(97,158,124)"
+      }
+    },
   },
   plugins: [],
 }
-
